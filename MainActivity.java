7ae16c0afab6cf80cf475fe44389e634d9af30c1
Miyamoto <hkanata@gmail.com>
2016-05-13 10:05:07 -0300

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,126 @@
+package br.com.tfleet.tfleetqrcode;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.IOException;
+
+public class MainActivity extends Activity {
+
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+    Intent takeVideoIntent;
+    VideoView mVideoView;
+
+    private Camera myCamera;
+    private MyCameraSurfaceView myCameraSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myCamera = getCameraInstance();
+        if(myCamera == null){
+            Toast.makeText(MainActivity.this,
+                    "Falha Na Camera",
+                    Toast.LENGTH_LONG).show();
+        }
+
+        myCameraSurfaceView = new MyCameraSurfaceView(this, myCamera);
+        //myCameraSurfaceView.setO
+        FrameLayout myCameraPreview = (FrameLayout)findViewById(R.id.videoview);
+        myCameraPreview.addView(myCameraSurfaceView);
+        
+
+    }
+
+
+    private Camera getCameraInstance(){
+        // TODO Auto-generated method stub
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    public class MyCameraSurfaceView extends SurfaceView implements SurfaceHolder.Callback{
+
+        private SurfaceHolder mHolder;
+        private Camera mCamera;
+
+        public MyCameraSurfaceView(Context context, Camera camera) {
+            super(context);
+            mCamera = camera;
+
+            // Install a SurfaceHolder.Callback so we get notified when the
+            // underlying surface is created and destroyed.
+            mHolder = getHolder();
+            mHolder.addCallback(this);
+            // deprecated setting, but required on Android versions prior to 3.0
+            mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int weight,
+                                   int height) {
+            // If your preview can change or rotate, take care of those events here.
+            // Make sure to stop the preview before resizing or reformatting it.
+
+            if (mHolder.getSurface() == null){
+                // preview surface does not exist
+                return;
+            }
+
+            // stop preview before making changes
+            try {
+                mCamera.stopPreview();
+            } catch (Exception e){
+                // ignore: tried to stop a non-existent preview
+            }
+
+            // make any resize, rotate or reformatting changes here
+
+            // start preview with new settings
+            try {
+                mCamera.setPreviewDisplay(mHolder);
+                mCamera.startPreview();
+
+            } catch (Exception e){
+            }
+        }
+
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            // TODO Auto-generated method stub
+            // The Surface has been created, now tell the camera where to draw the preview.
+            try {
+                mCamera.setPreviewDisplay(holder);
+                mCamera.startPreview();
+            } catch (IOException e) {
+            }
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // TODO Auto-generated method stub
+
+        }
+    }
+}
